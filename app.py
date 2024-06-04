1daca1e66d6e6d5bcfece2f31c4356bc3006f5a7
Vatsan <118588346+vatsan-murali@users.noreply.github.com>
2024-06-04 17:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def run_script():
         return jsonify({"error": "Failed to fetch Twitter trends"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
