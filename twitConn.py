d8d0569945fa9bf41b9f2b387aba534304676cdd
Vatsan <118588346+vatsan-murali@users.noreply.github.com>
2024-06-04 18:27:11 +0530

    Update twitConn.py

--- twitConn.py
+++ twitConn.py
@@ -25,7 +25,7 @@ def fetch_twitter_trends():
     })
 
     chrome_options = Options()
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
